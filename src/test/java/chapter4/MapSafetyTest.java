5c52d4e47b005ae4906625cc499d750c96d02958
ChristyWang <christy.tingting.wang@kingland.com>
2023-02-17 13:04:55 +0800

    Create MapSafetyTest.java

---
+++ src/test/java/chapter4/MapSafetyTest.java
@@ -0,0 +1,63 @@
+package chapter4;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MapSafetyTest {
+
+    @Test
+    public void unsafeHashMapTest() {
+        Map<String, Integer> map = new HashMap<>();
+
+        CompletableFuture<Void> future1 = CompletableFuture.runAsync(()-> put10000RandomValues(map));
+        CompletableFuture<Void> future2 = CompletableFuture.runAsync(()-> put10000RandomValues(map));
+
+        List<CompletableFuture<Void>> futures = List.of(future1, future2);
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()])).join();
+        assertEquals(map.size(), 20000);
+    }
+
+    @Test
+    public void safeImprovedMapTest() {
+        ImprovedMap<String, Integer> map = new ImprovedMap<>(new HashMap<>());
+
+        CompletableFuture<Void> future1 = CompletableFuture.runAsync(()-> put10000RandomValues(map));
+        CompletableFuture<Void> future2 = CompletableFuture.runAsync(()-> put10000RandomValues(map));
+
+        List<CompletableFuture<Void>> futures = List.of(future1, future2);
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()])).join();
+        assertEquals(map.size(), 20000);
+    }
+
+    private void put10000RandomValues(Map<String, Integer> map) {
+        IntStream.range(0,10000).forEach(number -> map.put(UUID.randomUUID().toString(), number));
+    }
+
+    private void put10000RandomValues(ImprovedMap<String, Integer> map) {
+        IntStream.range(0,10000).forEach(number -> map.put(UUID.randomUUID().toString(), number));
+    }
+}
+
+class ImprovedMap<K,V> {
+    private final HashMap<K,V> map;
+
+    public ImprovedMap(Map<K, V> map){
+        this.map = new HashMap<>(map);
+    }
+
+    public synchronized void put(K key, V value) {
+        map.put(key, value);
+    }
+
+    public synchronized int size() {
+        return this.map.size();
+    }
+}
